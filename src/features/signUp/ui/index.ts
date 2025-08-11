7f426c64a320a3486452f692eeae3973ace96e2b
김민조 <bluemayne0213@icloud.com>
2025-08-11 13:40:38 +0900

    CHORE(build): build 오류 수정

--- src/features/signUp/ui/index.ts
+++ src/features/signUp/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as SignUpForm } from './SignUpForm';
 export { default as Step1Form } from './Step1Form';
 export { default as Step2Form } from './Step2Form';
-export { default as FormInput } from './FormInput';
 export { default as AcademySelect } from './AcademySelect'; 
